39221c0392070be2450f41bd17c5db698b43362e
HimanshuKabra01 <kabrahimanshu123@gmail.com>
2025-01-17 16:02:37 +0530

    Add files via upload

---
+++ todo-list.js
@@ -0,0 +1,48 @@
+let todoList = [];
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-todo-input');
+  const name = inputElement.value;
+
+  const dueDateElement = document.querySelector('.js-due-date');
+
+  const dueDate = dueDateElement.value;
+  if (!name) {
+    alert('Please enter a todo')
+  } else if (!dueDate) {
+    alert('Please enter a due date');
+  } else {
+    todoList.push({
+      name,
+      dueDate
+    });
+  }
+  inputElement.value = '';
+  dueDateElement.value = '';
+  renderTodoList();
+}
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const {name, dueDate} = todoList[i];
+
+    let html = `
+    <div class="todo-list-row">
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button onclick="deleteTodo(${i});" class="delete-button">Delete</button>
+    </div>`
+    todoListHTML += html;
+  }
+
+  const todoElement = document.querySelector('.js-todo-list');
+
+  todoElement.innerHTML = todoListHTML;
+}
+
+function deleteTodo(index) {
+  todoList.splice(index, 1);
+  renderTodoList();
+}
